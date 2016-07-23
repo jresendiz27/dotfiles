80fc2025f9f735eed720efec56e83972da3ca140
Juan Alberto Resendiz Arteaga <jresendiz@nearsoft.com>
2016-07-23 12:43:42 -0500

    Fixing wrong link to gitconfig

--- createLinks.sh
+++ createLinks.sh
@@ -1,4 +1,4 @@
 ln -sf  ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf  ~/.dotfiles/.gitconfif ~/.gitconfig
+ln -sf  ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf  ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.tmuxinator ~
