fe6c78b82927d1fc3b481b7c060b8bf91245d80c
Juan Alberto Resendiz Arteaga <jresendiz@nearsoft.com>
2016-12-15 16:27:20 -0600

    Adding command for running pm

--- util.sh
+++ util.sh
@@ -28,7 +28,8 @@ alias gitPrettyLog='git log --graph --abbrev-commit --decorate --format=format:"
 alias gitBranchParent="git show-branch | grep '*' | grep -v '$(git rev-parse --abbrev-ref HEAD)' | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//'"
 alias gitRecentBranches='for k in `git branch | perl -pe s/^..//`; do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1`\\t$k; done | sort -r'
 alias mostUsedCommands="history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head"
-alias ls='ls --color=auto'
+alias runPM='java -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false  -Dusertransaction.class="com.skytouch.pm.config.SpringManualTransactionManager" -Dspring.config.location=$HOME/.conf-files/application-custom.yml -jar -Xdebug -Xrunjdwp:server=y,transport=dt_socket,suspend=n,address=3111 hos-pm/target/hos-pm-8.7.0.0-SNAPSHOT.war'
+# alias ls='ls --color=auto'
 alias ideaIBus='ibus-daemon -rd'
 ## JAVA OPTS
 #alias exportJavaOpts="export JAVA_OPTS='-server -Xms2048m -Xmx2048m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseParallelOldGC -XX:+UseAdaptiveSizePolicy -XX:+UseBiasedLocking'"
