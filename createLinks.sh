8701894e7e686806823368f6f31c8830a8201974
Juan Resendiz <jresendiz27@gmail.com>
2017-03-03 13:10:54 -0600

    Adding link to modified theme

--- createLinks.sh
+++ createLinks.sh
@@ -2,3 +2,4 @@ ln -sf  ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf  ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf  ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.tmuxinator ~
+ln -sf  ~/.dotfiles/jresendiz.zsh-theme ~/.oh-my-zsh/themes/jresendiz.zsh-theme
